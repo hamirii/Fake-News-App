b6735e7288d8ff9db5f5ddfd58a05328be337903
Haris <haris.amiri@live.com>
2020-12-24 17:14:59 -0800

    Revert "first commit on GitHub Desktop"
    This reverts commit 16101f9d88b9115bb4dcbddfca9c12f03d5fab45.

--- App.js
+++ App.js
@@ -1,9 +1,21 @@
-import * as React from "react";
-import { Text, View, StyleSheet } from "react-native";
-import Constants from "expo-constants";
-import Routes from "./Routes";
-import { Card } from "react-native-paper";
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
-  return <Routes />;
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
